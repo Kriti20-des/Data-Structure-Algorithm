7ba879eb1eef88ce1430efbe7a8f7566024a58f0
Kriti Kumari <kritikumarinke7@gmail.com>
2026-09-12 23:27:10 +0530

    Time: 0 ms (100%), Space: 42.4 MB (5.9%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+        int low = 1;
+        int high = num;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (mid == num / mid && num % mid == 0) {
+                return true;
+            }
+            else if (mid < num / mid) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
