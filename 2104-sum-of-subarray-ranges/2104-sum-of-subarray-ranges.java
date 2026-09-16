7e15a9d3637cd65e34d40abe37e8067d4c8b7c36
Kriti Kumari <kritikumarinke7@gmail.com>
2026-09-16 18:57:26 +0530

    Time: 25 ms (47.78%), Space: 46.5 MB (38.67%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -0,0 +1,108 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        long maxSum = sumOfMax(nums);
+        long minSum = sumOfMin(nums);
+
+        return maxSum - minSum;
+    }
+
+    public long sumOfMin(int[] arr) {
+
+        int n = arr.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                left[i] = i + 1;
+            } else {
+                left[i] = i - stack.peek();
+            }
+
+            stack.push(i);
+        }
+
+        stack.clear();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                right[i] = n - i;
+            } else {
+                right[i] = stack.peek() - i;
+            }
+
+            stack.push(i);
+        }
+
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += (long) arr[i] * left[i] * right[i];
+        }
+
+        return sum;
+    }
+
+    public long sumOfMax(int[] arr) {
+
+        int n = arr.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                left[i] = i + 1;
+            } else {
+                left[i] = i - stack.peek();
+            }
+
+            stack.push(i);
+        }
+
+        stack.clear();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                right[i] = n - i;
+            } else {
+                right[i] = stack.peek() - i;
+            }
+
+            stack.push(i);
+        }
+
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += (long) arr[i] * left[i] * right[i];
+        }
+
+        return sum; 
+    }
+}
