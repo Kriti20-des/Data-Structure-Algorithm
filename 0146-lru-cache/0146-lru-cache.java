72aa673b761ced6c5093c929b139ec652d6ebb00
Kriti Kumari <kritikumarinke7@gmail.com>
2026-09-19 17:52:54 +0530

    Time: 14 ms (93.48%), Space: 58.7 MB (73.02%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,105 @@
+class LRUCache {
+
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    HashMap<Integer, Node> map;
+
+    Node head;
+    Node tail;
+
+    int capacity;
+
+    public LRUCache(int capacity) {
+
+        this.capacity = capacity;
+
+        map = new HashMap<>();
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        Node node = map.get(key);
+
+        remove(node);
+        addToEnd(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        if (map.containsKey(key)) {
+
+            Node node = map.get(key);
+
+            node.value = value;
+
+            remove(node);
+            addToEnd(node);
+
+        } else {
+
+            Node newNode = new Node(key, value);
+
+            map.put(key, newNode);
+
+            addToEnd(newNode);
+
+            if (map.size() > capacity) {
+
+                Node lru = head.next;
+
+                remove(lru);
+
+                map.remove(lru.key);
+            }
+        }
+    }
+
+    public void remove(Node node) {
+
+        Node previous = node.prev;
+        Node nextNode = node.next;
+
+        previous.next = nextNode;
+        nextNode.prev = previous;
+    }
+
+    public void addToEnd(Node node) {
+
+        Node previous = tail.prev;
+
+        previous.next = node;
+        node.prev = previous;
+
+        node.next = tail;
+        tail.prev = node;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
